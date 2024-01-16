7f612ae8db99af1340f3a116b0f5ec6a2e202ca2
elz009 <144260155+elz009@users.noreply.github.com>
2024-01-16 13:19:26 +0600

    adding fixures for goods

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,20 @@
+"""
+URL configuration for app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-
-from django.urls import include, path
-
-app_name = 'name'
-urlpatterns = [
-    path('', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
-]   
-
 from django.urls import include, path
 
 
@@ -17,3 +24,10 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
 
+"""
+www.site.com/admin/
+www.site.com
+www.site.com/about/
+www.site.com/catalog/
+www.site.com/catalog/product
+"""
