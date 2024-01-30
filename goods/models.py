10dd69eeceaa4b3db57df1ee8cc1b09c6aa396ff
Elzar <elzarjoldoshev92@gmail.com>
2024-01-30 20:44:08 +0600

    Slug id

--- goods/models.py
+++ goods/models.py
@@ -31,5 +31,8 @@ class Products(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
-    def __str__(self):
-        return f'{self.name} Количество - {self.quantity}'
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price*self.discount/100, 2)
+        
+        return self.price
